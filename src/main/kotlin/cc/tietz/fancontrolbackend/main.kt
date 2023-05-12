a8fbafc5d32e6275b177883f188333e13be603fa
Leo Tietz <leo.tietz@gmail.com>
2023-05-12 16:27:39 +0200

    Add outdoor data recording endpoint

--- src/main/kotlin/cc/tietz/fancontrolbackend/main.kt
+++ src/main/kotlin/cc/tietz/fancontrolbackend/main.kt
@@ -12,10 +12,23 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
+import kotlinx.serialization.Serializable
+import java.time.Instant
+import kotlin.time.Duration.Companion.seconds
 
 fun main() {
     embeddedServer(Netty, port = 8080, module = Application::myApplicationModule).start(wait = true)
 }
+
+@Serializable
+data class OutdoorSensorRequest(
+    val temperature: Double,
+    val relativeHumidity: Int,
+)
+@Serializable
+data class OutdoorSensorResponse(
+    val sleepDurationMilliseconds: Int,
+)
 
 fun Application.myApplicationModule() {
     Database.initSchema()
@@ -30,6 +43,13 @@ fun Application.myApplicationModule() {
         post("/frontend/config") {
             Database.saveConfig(call.receive<PersistentConfig>())
             call.respond(Database.loadConfig())
+        }
+        post("/outdoor") {
+            val req = call.receive<OutdoorSensorRequest>()
+            Database.saveOutdoor(Database.OutdoorMeasurement(Instant.now(), req.temperature, req.relativeHumidity))
+            // todo: compute delay based on outdoor weather information
+            val delay = Database.loadConfig().pollingRateSensorOutside ?: 5.seconds
+            call.respond(OutdoorSensorResponse(delay.inWholeMilliseconds.toInt()))
         }
     }
 }
